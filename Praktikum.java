43d467cfb56cf3cca2f93b19175f32dd151783e8
SergeiA <as.antropov@yandex.ru>
2022-07-06 15:39:37 +0300

    Добавил вывод привет мир

--- Praktikum.java
+++ Praktikum.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        Join join = new Join();
-        join.Print();
+        System.out.println("Hello world!");
+        
     }
 
 
